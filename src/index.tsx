146a3502d8da63a0513f2b01b9128c88399160cb
newObjectccc <897333949@qq.com>
2024-03-18 09:53:11 +0800

    fix: react router addition

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from "@src/App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App></App>
+    </BrowserRouter>
+  </React.StrictMode>
 );
