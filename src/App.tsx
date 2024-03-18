ab3b12ad53c4aa34e1d9fa1a7c908325d2209e54
newObjectccc <897333949@qq.com>
2024-03-18 09:52:38 +0800

    fix: app layout example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import "./App.css";
-import React from "react";
+import { routes } from "@src/routes";
+import { Link, useRoutes } from "react-router-dom";
 
 const App = () => {
+  const routesElem = useRoutes(routes);
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <section className="border border-cyan-200 p-8 w-52 mx-auto my-8">
+        {routesElem}
+      </section>
+      <div className="flex justify-center">
+        <Link className="mr-8" to="/pageA">
+          Navigate to pageA
+        </Link>
+        <Link to="/pageB">Navigate to pageB</Link>
+      </div>
     </div>
   );
 };
