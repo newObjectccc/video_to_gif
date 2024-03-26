024f88731a4272d34afea171bda4c965d761bd52
newObjectccc <897333949@qq.com>
2024-03-27 00:12:14 +0800

    fix:  route update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,8 @@
 import Main from "@src/pages/Main";
-import { Navigate } from "react-router-dom";
 
 export const routes = [
   {
     path: "/",
-    children: [
-      { index: true, element: <Navigate to="main" /> },
-      { path: "main", element: <Main /> },
-    ],
+    element: <Main />,
   },
 ];
