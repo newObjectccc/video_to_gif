813e686d597d96ffaae9d92e3f3901d45143875e
newObjectccc <897333949@qq.com>
2024-03-29 09:51:59 +0800

    feat: spin animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,10 +67,16 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "spin-slow": {
+          "0%": { transform: "rotate(0deg)" },
+          "50%": { transform: "rotate(180deg)" },
+          "100%": { transform: "rotate(360deg)" }
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-slow": "spin-slow 2s ease-in-out infinite",
       },
     },
   },
