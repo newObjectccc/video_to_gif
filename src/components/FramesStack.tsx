01042b27bd3e2a2e8339ccd30d8ff79efadec481
newObjectccc <897333949@qq.com>
2024-04-01 15:06:48 +0800

    fix: frames stack migrate

---
+++ src/components/FramesStack.tsx
@@ -0,0 +1,157 @@
+import { ToastAction } from "@/components/ui/toast";
+import { toast } from "@/components/ui/use-toast";
+import { TransformStateContext } from "@src/App";
+import { getCurTargetElemIdx } from "@src/common/tools";
+import { ImgMenu } from "@src/components/ImgMenu";
+import mediumZoom from "medium-zoom";
+import { useTheme } from "next-themes";
+import React, {
+  MouseEventHandler,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
+
+interface FramesStackProps {
+  ref: any;
+}
+export const FramesStack: React.FC<React.PropsWithRef<FramesStackProps>> =
+  forwardRef((props, ref) => {
+    const [state, dispatch] = React.useContext(TransformStateContext);
+    const framesStackElemRef = useRef<HTMLDivElement>(null);
+    const progressRef = useRef<HTMLDivElement>(null);
+    const [curImageIdx, setCurImageIdx] = React.useState<number>(0);
+    const { theme } = useTheme();
+    const zoomRef = useRef<any>(null);
+    const { cacheFrames } = state;
+
+    const onMenuShow = (e: MouseEventHandler<HTMLDivElement>) => {
+      const idx = getCurTargetElemIdx(e);
+      setCurImageIdx(idx);
+    };
+
+    const onImgRemove = () => {
+      if (!cacheFrames.length) return;
+      cacheFrames.splice(curImageIdx, 1);
+      dispatch({ type: "cacheFrames", payload: cacheFrames } as any);
+      framesStackElemRef.current?.removeChild(
+        framesStackElemRef.current?.children[curImageIdx]
+      );
+    };
+
+    const onImgEdit = () => {
+      if (cacheFrames.length === 0) return;
+    };
+
+    const onImgPreview = () => {
+      const zoom = mediumZoom(
+        framesStackElemRef.current!.querySelectorAll("img")[curImageIdx],
+        { background: theme === "light" ? "#F8FAFC" : "#020817" }
+      );
+      zoom.open();
+    };
+
+    const transformImgDataToDom = (data: ImageData) => {
+      const framesStackElem = framesStackElemRef.current;
+      if (!framesStackElem) return;
+      if (cacheFrames.length === 0)
+        return toast({
+          title: "没有缓存帧",
+          description: "请检查左侧步骤是否有错误~",
+          action: <ToastAction altText="去上传">Undo</ToastAction>,
+        });
+      framesStackElemRef.current.innerHTML = "";
+      cacheFrames.forEach((imageData, idx) => {
+        setTimeout(() => {
+          const { width, height } = imageData;
+          const canvas = document.createElement("canvas");
+          canvas.width = width;
+          canvas.height = height;
+          const ctx = canvas.getContext("2d");
+          if (!ctx) return;
+          ctx.putImageData(imageData, 0, 0);
+          const img = document.createElement("img");
+          img.width = 120;
+          img.height = 60;
+          img.style.zIndex = "2"; // 因为 clipRect 的 z-index 是 1
+          img.decoding = "async";
+          img.loading = "lazy";
+          img.src = canvas.toDataURL();
+          framesStackElem.appendChild(img);
+          if (progressRef.current) {
+            progressRef.current.style.width = `${
+              ((idx + 1) / cacheFrames.length) * 100
+            }%`;
+            if (idx + 1 === cacheFrames.length) {
+              progressRef.current.style.visibility = "hidden";
+              zoomRef.current = mediumZoom(
+                framesStackElem.querySelectorAll("img"),
+                {
+                  background:
+                    theme === "light"
+                      ? "rgba(248, 250, 252, 0.85)"
+                      : "rgba(2, 8, 23, 0.85)",
+                }
+              );
+            }
+            if (idx === 0) progressRef.current.style.visibility = "visible";
+          }
+        });
+      });
+    };
+
+    useEffect(() => {
+      zoomRef.current?.update({
+        background:
+          theme === "light"
+            ? "rgba(248, 250, 252, 0.85)"
+            : "rgba(2, 8, 23, 0.85)",
+      });
+    }, [theme]);
+
+    useEffect(() => {
+      if (cacheFrames.length === 0 && framesStackElemRef.current)
+        framesStackElemRef.current.innerHTML = `<div
+              style="
+                display:flex;
+                align-items:center;
+                justify-content:center;
+                width:100%;
+                height:100%;
+              "
+            >
+              帧栈渲染区（如无其他细调操作，无需渲染帧栈，点击渲染采样帧按钮即可查看帧栈）
+            </div>`;
+    }, [cacheFrames]);
+
+    useImperativeHandle(ref, () => ({
+      renderStack: transformImgDataToDom,
+    }));
+
+    return (
+      <>
+        <ImgMenu
+          onRemove={onImgRemove}
+          onEdit={onImgEdit}
+          onPreview={onImgPreview}
+          className="flex flex-nowrap overflow-x-auto h-[130px] w-full mb-2 border border-dashed p-2"
+        >
+          <div
+            ref={framesStackElemRef}
+            className="flex flex-nowrap h-full gap-2 w-full"
+            onContextMenu={onMenuShow as any}
+          ></div>
+        </ImgMenu>
+        {cacheFrames.length ? (
+          <div className="text-sm">
+            共{cacheFrames.length}张关键帧（关键帧上右键单击弹出操作菜单）
+          </div>
+        ) : null}
+        <div
+          ref={progressRef}
+          className="h-1 my-4 flex items-center bg-slate-400 w-[1px] invisible"
+        ></div>
+      </>
+    );
+  });
